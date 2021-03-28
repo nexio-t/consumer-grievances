94a5975567d4ba78568bd8995760456169d6032c
Tomas <tomas.e.gear@gmail.com>
2021-03-27 22:11:07 -0400

    add create react app to frontend and first input field

---
+++ frontend/consumer-grievances/src/components/SearchInput.js
@@ -0,0 +1,100 @@
+/* eslint-disable no-use-before-define */
+// eslint-disable-next-line
+import React, { useState } from "react";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+export default function SearchInput() {
+
+// https://material-ui.com/components/autocomplete/
+
+const [value, setValue] = React.useState(null);
+const [inputValue, setInputValue] = React.useState('');
+const [options, setOptions] = React.useState([]);
+
+console.log("value is: ", value);  
+
+
+  // To do: Add some kind of container 
+  // To do: add button 
+  return (
+    <Autocomplete
+      id="combo-box-demo"
+      options={fullStateNames}
+      getOptionLabel={(option) => option.full}
+      style={{ width: 300 }}
+      onChange={(event, newValue) => {
+        console.log("newValue is: ", newValue); 
+        setOptions(newValue ? [newValue, ...options] : options);
+        setValue(newValue);
+      }}
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
+      renderInput={(params) => (
+        <TextField {...params} label="Combo box" variant="outlined" />
+      )}
+    />
+  );
+}
+
+//
+const fullStateNames = [
+    { name: "AK", full: "Alaska" },
+    { name: "AL", full: "Alabama" },
+    { name: "AR", full: "Arkansas" },
+    { name: "AZ", full: "Arizona" },
+    { name: "CA", full: "California" },
+    { name: "CO", full: "Colorado" },
+    { name: "CT", full: "Connecticut" },
+    { name: "DC", full: "Washington D.C." },
+    { name: "DE", full: "Delaware" },
+    { name: "FL", full: "Florida" },
+    { name: "GA", full: "Georgia" },
+    { name: "HI", full: "Hawaii" },
+    { name: "IA", full: "Iowa" },
+    { name: "ID", full: "Idaho" },
+    { name: "IL", full: "Illinois" },
+    { name: "IN", full: "Indiana" },
+    { name: "KS", full: "Kansas" },
+    { name: "KY", full: "Kentucky" },
+    { name: "LA", full: "Louisiana" },
+    { name: "MA", full: "Massachusetts" },
+    { name: "MD", full: "Maryland" },
+    { name: "ME", full: "Maine" },
+    { name: "MI", full: "Michigan" },
+    { name: "MN", full: "Minnesota" },
+    { name: "MO", full: "Missouri" },
+    { name: "MS", full: "Mississippi" },
+    { name: "MT", full: "Montana" },
+    { name: "NC", full: "North Carolina" },
+    { name: "ND", full: "North Dakota" },
+    { name: "NE", full: "Nebraska" },
+    { name: "NH", full: "New Hampshire" },
+    { name: "NJ", full: "New Jersey" },
+    { name: "NM", full: "New Mexico" },
+    { name: "NV", full: "Nevada" },
+    { name: "NY", full: "New York" },
+    { name: "OH", full: "Ohio" },
+    { name: "OK", full: "Oklahoma" },
+    { name: "OR", full: "Oregon" },
+    { name: "PA", full: "Pennsylvania" },
+    { name: "RI", full: "Rhode Island" },
+    { name: "SC", full: "South Carolina" },
+    { name: "SD", full: "South Dakota" },
+    { name: "TN", full: "Tennessee" },
+    { name: "TX", full: "Texas" },
+    { name: "UT", full: "Utah" },
+    { name: "VA", full: "Virginia" },
+    { name: "VT", full: "Vermont" },
+    { name: "WA", full: "Washington" },
+    { name: "WI", full: "Wisconsin" },
+    { name: "WV", full: "West Virginia" },
+    { name: "WY", full: "Wyoming" },
+    { name: "PR", full: "Puerto Rico" },
+    { name: "AS", full: "American Samoa" },
+    { name: "GU", full: "Guam" },
+    { name: "MP", full: "Northern Mariana Islands" },
+    { name: "VI", full: "U.S. Virgin Islands" }
+  ];
+
