a0be37915f291d0c7a1dc029b5705262aa8d1d09
Tomas <tomas.e.gear@gmail.com>
2021-03-31 23:18:14 -0400

    move search state function outside of hook

--- frontend/consumer-grievances/src/pages/GridLayout.js
+++ frontend/consumer-grievances/src/pages/GridLayout.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-use-before-define */
 // eslint-disable-next-line
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Box from "@material-ui/core/Box";
 import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
@@ -14,8 +14,8 @@ import DataTable from "../components/DataTable";
 import HideAppBar from "../components/HideAppBar";
 import ConsumerGrievances from "../assets/ConsumerGrievances_copy.png";
 import LoadingBar from "../components/LoadingBar";
-import { findStatePopulation, convertToThousands } from "../helpers/CleanData"
-import { fullStateNames } from "../data/data"; 
+import { findStatePopulation, convertToThousands } from "../helpers/CleanData";
+import { fullStateNames } from "../data/data";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -174,97 +174,89 @@ const renderContent = (
   );
 };
 
-export default function GridLayout() {
-  const classes = useStyles();
+const searchState2 = async (searchedState, setcomplaintCategories, settotalComplaints, setLoading, setcallsPer1000, settotalCalls) => {
+  
+  try {
+    const fetchPopData = await axios.get("/fetchPopulationData");
+    let statePopulation;
+    if (fetchPopData["status"] === 200) {
+      const {
+        data: { data },
+      } = fetchPopData;
+      statePopulation = await findStatePopulation(data, searchedState);
+    }
 
-  const searchState = async (inputValue) => {
-    setLoading(true);
-    setsearchedState(inputValue);
+    const fetchRobocalls = await axios.get(
+      `/fetchRobocallComplaints/${searchedState}`
+    );
 
-    try {
-      const fetchPopData = await axios.get("/fetchPopulationData");
-      let statePopulation;
-      if (fetchPopData["status"] === 200) {
-        const {
-          data: { data },
-        } = fetchPopData;
-        statePopulation = await findStatePopulation(data, inputValue);
+    let abbr = "";
+    
+    fullStateNames.map((state) => {
+      for (const x in state) {
+        if (state[x] === searchedState) return (abbr = state["abbr"]);
       }
+    });
 
-      const fetchRobocalls = await axios.get(
-        `/fetchRobocallComplaints/${inputValue}`
-      );
+    const fetchConsumerComplaints = await axios.get(
+      `/fetchConsumerComplaints/${abbr}`
+    );
 
-      let abbr = "";
-      const stateAbbr = fullStateNames.map((state) => {
-        for (const x in state) {
-          if (state[x] === inputValue) return (abbr = state["abbr"]);
-        }
-      });
-
-      const fetchConsumerComplaints = await axios.get(
-        `/fetchConsumerComplaints/${abbr}`
-      );
-
-      if (fetchConsumerComplaints["status"] === 200) {
-        const {
-          data: {
-            aggregations: {
-              product: {
-                product: { buckets },
-              },
+    if (fetchConsumerComplaints["status"] === 200) {
+      const {
+        data: {
+          aggregations: {
+            product: {
+              product: { buckets },
             },
           },
-        } = fetchConsumerComplaints;
-        const {
-          data: {
-            aggregations: {
-              product: { doc_count },
-            },
+        },
+      } = fetchConsumerComplaints;
+      const {
+        data: {
+          aggregations: {
+            product: { doc_count },
           },
-        } = fetchConsumerComplaints;
+        },
+      } = fetchConsumerComplaints;
 
-        const convertedData = convertToThousands(doc_count, statePopulation);
+      const convertedData = convertToThousands(doc_count, statePopulation);
 
-        if (doc_count && buckets) {
-          setcomplaintCategories(buckets);
-          settotalComplaints(convertedData);
-          setLoading(false);
-        } else {
-          setLoading(false);
-          console.log("no data to display for fetchConsumer complaints");
-        }
-      }
-
-      if (fetchRobocalls["status"] === 200) {
-        const {
-          data: { meta },
-        } = fetchRobocalls;
-        const totalRobocalls = fetchRobocalls.data.meta["record-total"];
-
-        const callsPerThousand = convertToThousands(
-          totalRobocalls,
-          statePopulation
-        );
-        const totalCalls = meta["record-total"];
-
-        if (callsPerThousand && totalCalls) {
-          setcallsPer1000(callsPerThousand);
-          settotalCalls(totalCalls);
-          setLoading(false);
-        }
+      if (doc_count && buckets) {
+        setcomplaintCategories(buckets);
+        settotalComplaints(convertedData);
+        setLoading(false);
       } else {
         setLoading(false);
-        console.log("no data to display for robocalls");
+        console.log("no data to display for fetchConsumer complaints");
       }
-
-      console.log("fetchPopData is: ", fetchPopData);
-      console.log("fetchRobocalls is: ", fetchRobocalls);
-    } catch (err) {
-      console.log("request error is: ", err);
-      throw new Error();
     }
-  };
+
+    if (fetchRobocalls["status"] === 200) {
+      const totalRobocalls = fetchRobocalls.data.meta["record-total"];
+      const callsPerThousand = convertToThousands(
+        totalRobocalls,
+        statePopulation
+      );
+
+      if (callsPerThousand && totalRobocalls) {
+        setcallsPer1000(callsPerThousand);
+        settotalCalls(totalRobocalls);
+        setLoading(false);
+      }
+    } else {
+      setLoading(false);
+      console.log("no data to display for robocalls");
+    }
+  } catch (err) {
+    console.log("request error is: ", err);
+    throw new Error();
+  }
+
+}
+
+export default function GridLayout() {
+  const classes = useStyles();
 
   const [searchedState, setsearchedState] = React.useState("");
   const [callsPer1000, setcallsPer1000] = React.useState(0);
@@ -275,15 +267,28 @@ export default function GridLayout() {
   const [error, setError] = React.useState(false);
   const [errorMsg, setErrorMsg] = React.useState("");
 
+
+  const isInitialMount = useRef(true);
+
+useEffect(() => {
+  if (isInitialMount.current) {
+     isInitialMount.current = false;
+  } else {
+    searchState2(searchedState, setcomplaintCategories, settotalComplaints, setLoading, setcallsPer1000, settotalCalls); 
+  }
+}, [searchedState]);
+
+  const searchState = async (inputValue) => {
+    setLoading(true);
+    setsearchedState(inputValue);
+  };
+
+ 
+
   return (
     <Container>
       <HideAppBar></HideAppBar>
-      <Grid
-        container
-        direction="column"
-        alignItems="center"
-        justify="center"
-      >
+      <Grid container direction="column" alignItems="center" justify="center">
         <Paper justify="center" className={classes.image} variant="outlined">
           <img width={"100%"} src={ConsumerGrievances} />
         </Paper>
@@ -293,6 +298,7 @@ export default function GridLayout() {
         <Grid item xs={10} md={8} lg={6}>
           <Paper className={classes.paper}>
             <SearchInput
+              func={setsearchedState}
               searchState={searchState}
               fullOptions={fullStateNames}
             />
